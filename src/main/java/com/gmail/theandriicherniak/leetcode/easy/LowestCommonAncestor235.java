c9633a0702dd239f4af6a43be61472e019a4e040
Andrii Cherniak <theandriicherniak@gmail.com>
2015-12-14 21:31:11 -0800

    lowest common ancestor

---
+++ src/main/java/com/gmail/theandriicherniak/leetcode/easy/LowestCommonAncestor235.java
@@ -0,0 +1,35 @@
+package com.gmail.theandriicherniak.leetcode.easy;
+
+/**
+ * Created by andriicherniak on 12/14/15.
+ */
+
+public class LowestCommonAncestor235 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(root.val > p.val && root.val < q.val){
+            return root;
+        }else if(root.val > q.val && root.val < p.val){
+            return root;
+        }
+        else if(root.val>p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }else if(root.val<p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        return root;
+    }
+}
