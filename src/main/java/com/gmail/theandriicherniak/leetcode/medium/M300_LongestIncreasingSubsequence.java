f96662f8f8536acd2fed147ebf88f493575648b6
Andrii Cherniak <theandriicherniak@gmail.com>
2016-03-15 16:30:43 -0700

    Longest Increasing Subsequence : without hashmap

--- src/main/java/com/gmail/theandriicherniak/leetcode/medium/M300_LongestIncreasingSubsequence.java
+++ src/main/java/com/gmail/theandriicherniak/leetcode/medium/M300_LongestIncreasingSubsequence.java
@@ -3,26 +3,25 @@ package com.gmail.theandriicherniak.leetcode.medium;
 /**
  * Created by andriicherniak on 3/15/16.
  */
-import java.util.*;
-
 public class M300_LongestIncreasingSubsequence {
     public int lengthOfLIS(int[] nums) {
         int L = nums.length;
         if (L == 0) return 0;
         if (L == 1) return 1;
-        HashMap<Integer, Integer> subsequence_set = new HashMap<Integer, Integer>();
+        int [] subsequence_len = new int[L];
         int maxL = 0;
 
-        for (int i = L - 1; i >= 0; i--){
-            int v = nums[i];
-            int vL = 1;
-            for (int vv : subsequence_set.keySet()){
-                if (vv > v) vL = Math.max(vL, subsequence_set.get(vv) + 1);
-            }
-//            System.out.println(" v " + v + " vL " + vL);
+        for (int i = 0; i < L; i++){
+            int bestL = 1;
 
-            subsequence_set.put(v, vL);
-            maxL = Math.max(maxL, vL);
+            for (int j = 0; j < i; j++){
+                if (nums[j] < nums[i]){
+                    bestL = Math.max(bestL, subsequence_len[j] + 1);
+                }
+            }
+
+            subsequence_len[i] = bestL;
+            maxL = Math.max(maxL, bestL);
         }
         return maxL;
     }
