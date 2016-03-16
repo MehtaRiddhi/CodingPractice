4cb8c3e4af29b3a720c151c7d5f9a07e4df474df
Andrii Cherniak <theandriicherniak@gmail.com>
2016-03-16 13:32:40 -0700

    Median of Two Sorted Arrays

--- src/main/java/com/gmail/theandriicherniak/leetcode/hard/H4_MedianTwoSortedArrays.java
+++ src/main/java/com/gmail/theandriicherniak/leetcode/hard/H4_MedianTwoSortedArrays.java
@@ -4,53 +4,58 @@ package com.gmail.theandriicherniak.leetcode.hard;
  * Created by andriicherniak on 3/8/16.
  */
 public class H4_MedianTwoSortedArrays {
-    private int isKthElement(int[] A, int[] B, int La, int Lb, int k, int Apos){
-        System.out.println(" k " + k + " Apos " + Apos);
-        
-        if (Apos > k) return 1;
-        int cnt = k - Apos;
-        if (cnt == 0) return 0;
-
-        int i = 0;
-
-        while (i < Lb && cnt >= 0){
-            if (B[i] < A[Apos]) cnt --;
-            i++;
-        }
-
-        if (cnt < 0) return -1;
-
-        while (i < Lb && cnt > 0){
-            if (B[i] == A[Apos]) cnt --;
-            if (cnt == 0) return 0;
-            i++;
-        }
-
-        if (cnt > 0) return 1;
-
-        return 0;
+    private double medianSingleArray(int [] nums, int L){
+        int medId1 = L / 2;
+        int medId2 = (L % 2 == 1) ? L / 2 : (L / 2 - 1);
+        return 0.5 * (nums[medId1] + nums[medId2]);
     }
 
-    private int findKthElement(int[] A, int[] B, int La, int Lb, int k, int from, int to){
-        for (int pos = from; pos <= to; pos++){
-            System.out.println(pos + " " + A[pos] + " " + isKthElement(A, B, La, Lb, k, pos));
+    private boolean leStats(int [] nums, int L, int v, int leOrder){
+        if (leOrder > L) return false;
+        if (leOrder == 0) {
+            if (nums[0] >= v) return true;
+            return false;
         }
-        return 0;
+        if (leOrder == L){
+            if (nums[L - 1] <= v) return true;
+            return false;
+        }
+        if (nums[leOrder - 1] <= v && v <= nums[leOrder]) return true;
+        return false;
+    }
+    private int getKthElement(int[] nums1, int[] nums2, int L1, int L2, int K){
+        for (int i = 0; i < Math.min(K, L1); i++){
+            int residualK = K - i - 1;
+            boolean satisfiedConstraint = leStats(nums2, L2, nums1[i], residualK);
+            if (satisfiedConstraint) return nums1[i];
+        }
+
+        for (int i = 0; i < Math.min(K, L2); i++){
+            int residualK = K - i - 1;
+            boolean satisfiedConstraint = leStats(nums1, L1, nums2[i], residualK);
+            if (satisfiedConstraint) return nums2[i];
+        }
+
+        return nums1[0];
     }
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int L1 = nums1.length;
         int L2 = nums2.length;
-        if (L1 == 0 && L2 == 0) return 0.0;
-        if (L1 != 0 && L2 == 0){
-            return 0.5 * (nums1[L1/2] + nums1[L1/2 - (1 - L1 % 2)]);
-        }
-        if (L1 == 0 && L2 != 0){
-            return 0.5 * (nums2[L2/2] + nums2[L2/2 - (1 - L2 % 2)]);
-        }
 
-        findKthElement(nums1, nums2, L1, L2, (L1 + L2) / 2, 0, L1 - 1);
+        if (L1 == 0 && L2 == 0) return 0;
+        if (L1 == 0 && L2 > 0) return medianSingleArray(nums2, L2);
+        if (L1 > 0 && L2 == 0) return medianSingleArray(nums1, L1);
 
-        return 0.0;
+        int L3 = L1 + L2;
+        int medId1 = L3 / 2;
+        int medId2 = (L3 % 2 == 1) ? L3 / 2 : (L3 / 2 - 1);
 
+        if (L3 % 2 == 1) return getKthElement(nums1, nums2, L1, L2, medId1 + 1);
+
+        int m1 = getKthElement(nums1, nums2, L1, L2, medId1 + 1);
+        int m2 = getKthElement(nums1, nums2, L1, L2, medId2 + 1);
+
+
+        return 0.5 * (m1 + m2);
     }
 }
--- src/main/java/com/gmail/theandriicherniak/leetcode/medium/Main.java
+++ src/main/java/com/gmail/theandriicherniak/leetcode/medium/Main.java
@@ -4,13 +4,9 @@ package com.gmail.theandriicherniak.leetcode.medium;
 /**
  * Created by andriicherniak on 11/23/15.
  */
-public class Main {
-    public static void main(java.lang.String[] args) {
-        System.out.println("running");
-
-
-        M275_HIndexII h = new M275_HIndexII();
-        System.out.println(h.hIndex(new int[]{0, 1, 3, 5, 6}));
-
-    }
-}
+//public class Main {
+//    public static void main(java.lang.String[] args) {
+//        System.out.println("running");
+//
+//    }
+//}
