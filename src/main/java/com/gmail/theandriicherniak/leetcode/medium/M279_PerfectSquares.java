f92922fe4808514b710f9a3571c8d6bb8de94851
Andrii Cherniak <theandriicherniak@gmail.com>
2016-04-13 16:32:05 -0700

    Perfect Squares : speedup

--- src/main/java/com/gmail/theandriicherniak/leetcode/medium/M279_PerfectSquares.java
+++ src/main/java/com/gmail/theandriicherniak/leetcode/medium/M279_PerfectSquares.java
@@ -8,24 +8,25 @@ public class M279_PerfectSquares {
         if (n <= 0) return 0;
         if (n == 1) return 1;
 
-        int k = (int) Math.floor(Math.sqrt(n));
-        int [] numbers = new int[k];
-        for (int i = 1; i <= k; i++) numbers[i-1] = i * i;
+        int k;
 
-        int [] buffer = new int[n + 1];
+        int[] buffer = new int[n + 1];
         int vv;
         int minL;
 
-        for (int i = 0; i < k; i++) buffer[numbers[i]] = 1;
+        buffer[1] = 1;
 
-        for (int v = 2; v <= n; v++){
-            for (int i = 0; i < k; i++){
-                vv = v - numbers[i];
-                if (vv >= 1){
-                    minL = 1 + buffer[vv];
-                    if (buffer[v] == 0) buffer[v] = minL;
-                    else buffer[v] = Math.min(buffer[v], minL);
-                }
+        for (int v = 2; v <= n; v++) {
+            k = (int) Math.floor(Math.sqrt(v));
+            if (v == k * k) {
+                buffer[v] = 1;
+                continue;
+            }
+            for (int i = 1; i <= k; i++) {
+                vv = v - i * i;
+                minL = 1 + buffer[vv];
+                if (buffer[v] == 0) buffer[v] = minL;
+                else buffer[v] = Math.min(buffer[v], minL);
             }
         }
 
