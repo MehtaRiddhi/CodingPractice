ac87cca02472b10d1646cf17ad9beeb29ce73e39
Andrii Cherniak <theandriicherniak@gmail.com>
2016-04-14 13:30:41 -0700

    Maximal Square : update

--- src/main/java/com/gmail/theandriicherniak/leetcode/medium/M221_MaximalSquare.java
+++ src/main/java/com/gmail/theandriicherniak/leetcode/medium/M221_MaximalSquare.java
@@ -4,50 +4,44 @@ package com.gmail.theandriicherniak.leetcode.medium;
  * Created by andriicherniak on 4/13/16.
  */
 public class M221_MaximalSquare {
-    private int helper(int [][] buffer, int r1, int c1, int r2, int c2){
-        return buffer[r2][c2] - buffer[r1-1][c2] - (buffer[r2][c1-1] - buffer[r1-1][c1-1]);
-    }
+
     public int maximalSquare(char[][] matrix) {
         int R = matrix.length;
         if (R == 0) return 0;
         int C = matrix[0].length;
         if (C == 0) return 0;
 
-        int [][] buffer = new int[R+1][C+1];
-        for (int r = 1; r <= R; r ++){
-            for (int c = 1; c <= C; c++){
-                buffer[r][c] = Character.getNumericValue(matrix[r-1][c-1]) + (buffer[r-1][c] - buffer[r-1][c-1]) + buffer[r][c-1];
-            }
+        int L;
+
+        int [][] buffer = new int[R][C];
+        int maxL = 0;
+        int L1;
+        int L2;
+
+        for (int c = 0; c < C; c++){
+            buffer[0][c] = matrix[0][c] - '0';
+            maxL = Math.max(maxL, buffer[0][c]);
         }
 
-        int rNext;
-        int cNext;
-        int aNext;
-        int sNext;
+        for (int r = 0; r < R; r++){
+            buffer[r][0] = matrix[r][0] - '0';
+            maxL = Math.max(maxL, buffer[r][0]);
+        }
 
-        int result = 0;
-
-        for (int r = 1; r <= R; r ++){
-            for (int c = 1; c <= C; c++){
-                if (matrix[r-1][c-1] == '1'){
-
-                    result = Math.max(result, 1);
-
-                    rNext = r + 1;
-                    cNext = c + 1;
-
-                    while (rNext <= R && cNext <= C && matrix[rNext - 1][cNext - 1] == '1'){
-                        aNext = (rNext - r + 1)*(rNext - r + 1);
-                        sNext = helper(buffer, r, c, rNext, cNext);
-                        if (aNext == sNext) result = Math.max(result, aNext);
-
-                        rNext += 1;
-                        cNext += 1;
-                    }
+        for (int r = 1; r < R; r ++){
+            for (int c = 1; c < C; c++){
+                L = matrix[r][c] - '0';
+                if (L == 0) buffer[r][c] = 0;
+                else {
+                    L1 = buffer[r-1][c];
+                    L2 = buffer[r][c-1];
+                    if (L1 != L2) buffer[r][c] = Math.min(L1, L2) + 1;
+                    else buffer[r][c] = (matrix[r-L1][c-L1] == '1') ? L1 + 1 : L1;
                 }
+                maxL = Math.max(maxL, buffer[r][c]);
             }
         }
-        
-        return result;
+
+        return maxL * maxL;
     }
 }
--- src/main/java/com/gmail/theandriicherniak/leetcode/medium/Main.java
+++ src/main/java/com/gmail/theandriicherniak/leetcode/medium/Main.java
@@ -9,7 +9,11 @@ public class Main {
         System.out.println("running medium ");
 
         M221_MaximalSquare s = new M221_MaximalSquare();
-        System.out.println(s.maximalSquare(new char[][]{"11".toCharArray(), "11".toCharArray()}));
+        System.out.println(s.maximalSquare(new char[][]{
+                "1010".toCharArray(),
+                "1011".toCharArray(),
+                "1011".toCharArray(),
+                "1111".toCharArray()}));
 
     }
 }
