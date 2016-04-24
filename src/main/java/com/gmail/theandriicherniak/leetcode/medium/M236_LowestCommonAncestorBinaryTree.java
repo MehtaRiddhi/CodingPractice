a853e705840206c1fb85ab66eb328a1eab7a6f1b
Andrii Cherniak <theandriicherniak@gmail.com>
2016-04-23 22:48:33 -0700

    Lowest Common Ancestor of a Binary Tree : speedup

--- src/main/java/com/gmail/theandriicherniak/leetcode/medium/M236_LowestCommonAncestorBinaryTree.java
+++ src/main/java/com/gmail/theandriicherniak/leetcode/medium/M236_LowestCommonAncestorBinaryTree.java
@@ -16,53 +16,12 @@ public class M236_LowestCommonAncestorBinaryTree {
         }
     }
 
-    private int DFShelper(TreeNode root, TreeNode t, ArrayList<TreeNode> path) {
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        Stack<Integer> level = new Stack<Integer>();
-
-        stack.add(root);
-        level.add(1);
-
-        TreeNode next;
-        int curLevel;
-
-        while (!stack.empty()){
-            next = stack.pop();
-            curLevel = level.pop();
-            if (curLevel > path.size()) path.add(next);
-            else path.set(curLevel - 1, next);
-
-            if (next == t) return curLevel;
-            if (next.right != null) {
-                stack.add(next.right);
-                level.add(curLevel + 1);
-            }
-            if (next.left != null) {
-                stack.add(next.left);
-                level.add(curLevel + 1);
-            }
-        }
-
-        return 0;
-    }
-
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) return null;
-        ArrayList<TreeNode> path_p = new ArrayList<TreeNode>();
-        ArrayList<TreeNode> path_q = new ArrayList<TreeNode>();
-
-        TreeNode ancestor = root;
-
-
-        int Lp = DFShelper(root, p, path_p);
-        int Lq = DFShelper(root, q, path_q);
-
-        for (int i = 0; i < Math.min(Lp, Lq); i++){
-            if (path_p.get(i) == path_q.get(i)) ancestor = path_p.get(i);
-        }
-
-
-        return ancestor;
+        if (root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left != null && right != null ? root : left == null ? right : left;
     }
 }
