eb32d67d3d9f57e267de8f4b9aef2f47108100a5
Andrii Cherniak <theandriicherniak@gmail.com>
2016-04-28 19:59:48 -0700

    Populating Next Right Pointers in Each Node II

--- src/main/java/com/gmail/theandriicherniak/leetcode/hard/H117_PopulatingNextRightPointersII.java
+++ src/main/java/com/gmail/theandriicherniak/leetcode/hard/H117_PopulatingNextRightPointersII.java
@@ -14,5 +14,28 @@ public class H117_PopulatingNextRightPointersII {
     }
 
     public void connect(TreeLinkNode root) {
+        TreeLinkNode cur = root;
+        TreeLinkNode nextRowHead = null;
+        TreeLinkNode nextRowCur = null;
+
+
+        while (cur != null){
+            nextRowHead = new TreeLinkNode(0);
+            nextRowCur = nextRowHead;
+
+            while (cur != null){
+                if (cur.left != null){
+                    nextRowCur.next = cur.left;
+                    nextRowCur = cur.left;
+                }
+                if (cur.right != null){
+                    nextRowCur.next = cur.right;
+                    nextRowCur = cur.right;
+                }
+                cur = cur.next;
+            }
+            cur = nextRowHead.next;
+        }
+
     }
 }
