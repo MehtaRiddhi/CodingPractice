cc4f0fc29c5bbf5e58e94d6bc3c73254dcc7b70a
Andrii Cherniak <theandriicherniak@gmail.com>
2016-05-04 15:25:06 -0700

    Paint House II : speedup

--- src/main/java/com/gmail/theandriicherniak/leetcode/hard/H265_PaintHouseII.java
+++ src/main/java/com/gmail/theandriicherniak/leetcode/hard/H265_PaintHouseII.java
@@ -5,44 +5,42 @@ package com.gmail.theandriicherniak.leetcode.hard;
  */
 public class H265_PaintHouseII {
 
+    private static void minExclude(int [] input, int [] output){
+        output[0] = Integer.MAX_VALUE;
+        for (int c = 1; c <= input.length - 1; c++){
+            output[c] = Math.min(output[c-1], input[c-1]);
+        }
+        int minRev = Integer.MAX_VALUE;
+        for (int c = input.length - 2; c >= 0; c--){
+            minRev = Math.min(minRev, input[c+1]);
+            output[c] = Math.min(output[c], minRev);
+        }
+    }
+
     public int minCostII(int[][] costs) {
         int L = costs.length;
         if (L == 0) return 0;
         int C = costs[0].length;
         if (C == 0) return 0;
 
-        int[][] buffer = new int[L][C];
+        int[] buffer = new int[C];
+        int [] minExcludeOutput = new int[C];
 
-        int prevMinCost = Integer.MAX_VALUE;
-        int curMinCost;
-
-        for (int c = 0; c < C; c++){
-            buffer[0][c] = costs[0][c];
-            prevMinCost = Math.min(prevMinCost, costs[0][c]);
-        }
+        for (int c = 0; c < C; c++) buffer[c] = costs[0][c];
 
         for (int i = 1; i < L; i++){
-            curMinCost = Integer.MAX_VALUE;
+            minExclude(buffer, minExcludeOutput);
 
-            for (int c = 0; c < C; c ++){
-                if (buffer[i-1][c] > prevMinCost) buffer[i][c] = prevMinCost + costs[i][c];
-                else {
-                    int minV = Integer.MAX_VALUE;
-                    for (int cc = 0; cc < C; cc ++){
-                        if (c != cc) minV = Math.min(minV, buffer[i-1][cc]);
-                    }
-                    buffer[i][c] = minV + costs[i][c];
-                }
-                curMinCost = Math.min(curMinCost, buffer[i][c]);
+            for (int c = 0; c < C; c++){
+                buffer[c] = minExcludeOutput[c] + costs[i][c];
             }
-            prevMinCost = curMinCost;
         }
 
 
         int minCost = Integer.MAX_VALUE;
 
         for (int i = 0; i < C; i++){
-            minCost = Math.min(minCost, buffer[L - 1][i]);
+            minCost = Math.min(minCost, buffer[i]);
         }
 
         return minCost;
