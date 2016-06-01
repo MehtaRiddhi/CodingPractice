8c987d5094ed392280c20746189661f94baab80f
Andrii Cherniak <theandriicherniak@gmail.com>
2016-05-31 20:43:40 -0700

    Longest Valid Parentheses

--- src/main/java/com/gmail/theandriicherniak/leetcode/hard/H32_LongestValidParentheses.java
+++ src/main/java/com/gmail/theandriicherniak/leetcode/hard/H32_LongestValidParentheses.java
@@ -4,18 +4,33 @@ package com.gmail.theandriicherniak.leetcode.hard;
  * Created by andriicherniak on 5/31/16.
  */
 public class H32_LongestValidParentheses {
-    private int lonestParenthesis(char [] ar, int L, int start){
-        int counter = 0;
-        int pos = start;
-        while (pos < L && counter >= 0){
-
-        }
-        return pos - start;
-    }
 
     public int longestValidParentheses(String s) {
         int L = s.length();
         if (L == 0) return 0;
-        return L;
+        char[] ar = s.toCharArray();
+
+        int maxL = 0;
+        int left = 0;
+
+        int [] result = new int[L + 1];
+        int [] start = new int[L + 1];
+
+        for (int i = 0; i < L; i++){
+            if (ar[i] == '('){
+                start[++left] = i;
+            }else {
+                if (left > 0){
+                    result[i] = result[i - 1] + 2;
+                    if (start[left] > 0) result[i] += result[start[left] - 1];
+                    maxL = Math.max(maxL, result[i]);
+                    left --;
+                }else {
+                    result[i] = 0;
+                }
+            }
+        }
+
+        return maxL;
     }
 }
--- src/main/java/com/gmail/theandriicherniak/leetcode/hard/Main.java
+++ src/main/java/com/gmail/theandriicherniak/leetcode/hard/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(java.lang.String[] args) {
         System.out.println("running hard examples");
 
-        H224_BasicCalculator calculator = new H224_BasicCalculator();
-        System.out.println(calculator.calculate("(1+(4+5+2)-3)+(6+8)"));
+        H32_LongestValidParentheses p = new H32_LongestValidParentheses();
+        System.out.println(p.longestValidParentheses("()(()"));
     }
 }
