1cff7857b8cc0b7dc39dea312141955a3da8035c
Andrii Cherniak <theandriicherniak@gmail.com>
2016-06-20 09:21:11 -0700

    update

---
+++ src/main/java/com/gmail/theandriicherniak/leetcode/medium/M94_BinaryTreeMorris.java
@@ -0,0 +1,20 @@
+package com.gmail.theandriicherniak.leetcode.medium;
+
+/**
+ * Created by andriicherniak on 6/16/16.
+ */
+
+import java.util.*;
+
+public class M94_BinaryTreeMorris {
+     public class TreeNode {
+             int val;
+             TreeNode left;
+             TreeNode right;
+             TreeNode(int x) { val = x; }
+        }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        return null;
+    }
+}
